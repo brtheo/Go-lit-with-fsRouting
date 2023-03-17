e44748231f5d59bc52795330b68e7472decf7d5b
Edouard-Depret <edouarddepret@gmail.com>
2023-03-17 14:56:58 +0100

    remove greet()

--- app.go
+++ app.go
@@ -49,13 +49,7 @@ func NewApp() *App {
 // startup is called when the app starts. The context is saved
 // so we can call the runtime methods
 func (a *App) startup(ctx context.Context) {
-	a.GetRoutes()
 	a.ctx = ctx
-}
-
-// Greet returns a greeting for the given name
-func (a *App) Greet(name string) string {
-	return fmt.Sprintf("Hello %s, It's show time!", name)
 }
 
 func (a *App) GetRoutes() (routes []string) {
